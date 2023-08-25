29893a77c972c8cd8bf53ac3b9ec7256c5bac616
harsha201000 <138086385+harsha201000@users.noreply.github.com>
2023-08-24 21:23:40 -0500

    Add files via upload
    This is My First Website using HTML, CSS, and JavaScript.

---
+++ MyFirstWebsite/main.js
@@ -0,0 +1,8 @@
+
+// Hello World Program in JavaScript
+console.log("Hello World");
+console.log("Welcome to JavaScript");
+console.log("Harsha First Java Program");
+console.log("I am 13 years old.");
+console.log("I am in grade 8");
+console.log("I have created a cool Webpage in HTML");
